3e46bd866962a54ec20eb37a97149fa269b861fc
maxrosario99 <maxumum10000@gmail.com>
2024-04-04 18:52:01 -0400

    Set up

---
+++ src/components/Cert.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Cert = () => {
+  return (
+    <div>Cert
+
+        <h1>Exam 762: SQL Databases</h1>
+        <h2>Microsoft</h2>
+        <h3>Issued May 2020</h3>
+        <button>Edit Certification</button>
+    </div>
+  )
+}
+
+export default Cert
---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Cert from './Cert'
+
+
+const Layout = () => {
+  return (
+    <div>Layout
+
+        <div id="cert-container">
+            <Cert />
+        </div>
+    </div>
+  )
+}
+
+export default Layout
